15a69483ac677c3fbb64619bbc7fc0dd6873aab4
Afeef <afeef.badri@gmail.com>
2022-03-21 17:08:34 +0100

    first test code medCoupling

--- cpp/medCoupling.cpp
+++ cpp/medCoupling.cpp
@@ -23,6 +23,47 @@
 #include <iostream>
 
 #include "MEDLoader.hxx"
+#include "MEDLoaderBase.hxx"
+#include "MEDCouplingUMesh.hxx"
+#include "MEDCouplingFieldDouble.hxx"
+#include "MEDCouplingFieldFloat.hxx"
+#include "MEDCouplingMemArray.hxx"
 
 using namespace std;
 using namespace MEDCoupling;
+
+int testRun()
+{
+  double targetCoords[24]={
+    -0.3,-0.3, 0.2,-0.3, 0.7,-0.3, -0.3,0.2, 0.2,0.2, 0.7,0.2, -0.3,0.7, 0.2,0.7, 0.7,0.7,
+    -0.05,0.95, 0.2,1.2, 0.45,0.95
+  };
+  mcIdType targetConn[24]={1,4,2, 4,5,2, 6,10,8,9,11,7, 0,3,4,1, 6,7,4,3, 7,8,5,4};
+  MEDCouplingUMesh *targetMesh=MEDCouplingUMesh::New();
+  targetMesh->setMeshDimension(2);
+  targetMesh->allocateCells(5);
+  targetMesh->setName("2DMesh_2");
+  targetMesh->insertNextCell(INTERP_KERNEL::NORM_TRI3,3,targetConn);
+  targetMesh->insertNextCell(INTERP_KERNEL::NORM_TRI3,3,targetConn+3);
+  targetMesh->insertNextCell(INTERP_KERNEL::NORM_QUAD4,4,targetConn+12);
+  targetMesh->insertNextCell(INTERP_KERNEL::NORM_QUAD4,4,targetConn+16);
+  targetMesh->insertNextCell(INTERP_KERNEL::NORM_TRI6,6,targetConn+6);
+  targetMesh->finishInsertingCells();
+  DataArrayDouble *myCoords=DataArrayDouble::New();
+  myCoords->alloc(12,2);
+  myCoords->setInfoOnComponent(0,"toto [m]");
+  myCoords->setInfoOnComponent(1,"energie [kW]");
+  std::copy(targetCoords,targetCoords+24,myCoords->getPointer());
+  targetMesh->setCoords(myCoords);
+  myCoords->decrRef();
+  WriteUMesh("file2.med",targetMesh,true);
+  return 1;
+}
+
+int main(){
+
+int err = testRun();
+return 1;
+}
+
+
